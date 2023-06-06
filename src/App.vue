2c6f2d571fe4e27ea79a43b435fd72653f10229e
Kauê <kauelinkin@gmail.com>
2023-06-06 11:05:57 -0300

    templates vue e funcao para o jogo completa

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h1>Jogo Ímpar ou Par</h1>
+
+    <label for="playerInput">Escolha um número de 1 a 6:</label>
+    <input type="number" id="playerInput" v-model="playerNumber" min="1" max="6">
+
+    <button id="playButton" @click="playGame">Jogar!</button>
+
+    <div id="result">
+      <p>Resultado: {{ result }}</p>
+      <p>{{ gameResult }}</p>
+      <p v-if="gameResult === 'Você venceu!'">Número escolhido pela máquina: {{ machineNumber }}</p>
+      <p v-if="errorMessage">{{ errorMessage }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      playerNumber: null,
+      machineNumber: null,
+      result: '',
+      gameResult: '',
+      errorMessage: ''
+    };
+  },
+  methods: {
+    generateMachineNumber() {
+      return Math.floor(Math.random() * 6) + 1;
+    },
+    playGame() {
+      if (this.playerNumber < 1 || this.playerNumber > 6) {
+        this.errorMessage = 'Escolha um número entre 1 e 6.';
+        return;
+      }
+
+      this.machineNumber = this.generateMachineNumber();
+      const total = this.playerNumber + this.machineNumber;
+      const isEven = total % 2 === 0;
+
+      if (isEven) {
+        this.result = 'Par';
+      } else {
+        this.result = 'Ímpar';
+      }
+
+      if (
+        (isEven && this.playerNumber % 2 === 0) ||
+        (!isEven && this.playerNumber % 2 !== 0)
+      ) {
+        this.gameResult = 'Você venceu!';
+      } else {
+        this.gameResult = 'Você perdeu!';
+      }
+
+      this.errorMessage = '';
+    }
+  }
+}
+</script>
